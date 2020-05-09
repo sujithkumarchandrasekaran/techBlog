114a69def1f7e845b43964b4e47e79c7615f5df2
Sujith <sujithchandrasekaran@gmail.com>
2020-05-09 23:32:18 +0530

    after registration and loginuser are implemented

--- src/main/java/technicalblog/controller/UserController.java
+++ src/main/java/technicalblog/controller/UserController.java
@@ -1,12 +1,25 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import technicalblog.model.Post;
 import technicalblog.model.User;
+import technicalblog.service.PostService;
+import technicalblog.service.UserService;
+
+import java.util.ArrayList;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("users/login")
     public String login() {
@@ -20,7 +33,24 @@ public class UserController {
 
     @RequestMapping (value="users/login", method= RequestMethod.POST)
     public String loginuser (User user) {
-        return "redirect:/posts";
+        if(userService.login(user)) {
+            return "redirect:/posts";
+        }
+        else {
+            return "users/login";
+        }
+    }
+
+    @RequestMapping (value="users/logout", method=RequestMethod.POST)
+    public String logout (Model model) {
+        ArrayList<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts",posts);
+        return "index";
+    }
+
+    @RequestMapping(value = "users/registration", method=RequestMethod.POST)
+    public String registerUser(User user) {
+        return "users/login";
     }
 
 }
